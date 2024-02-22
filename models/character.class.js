019ca9d2381d489b78abda6136053ac5663f24bf
Cedric Daunke <daunke40@gmail.com>
2024-02-22 18:36:53 +0100

    added idle animation and enemy movement

--- models/character.class.js
+++ models/character.class.js
@@ -1,10 +1,44 @@
 class Character extends moveableObject {
     height = 200;
     width = 250;
+    IMAGES_IDLE = [
+        '../img/1.Sharkie/1.IDLE/2.png',
+        '../img/1.Sharkie/1.IDLE/3.png',
+        '../img/1.Sharkie/1.IDLE/4.png',
+        '../img/1.Sharkie/1.IDLE/5.png',
+        '../img/1.Sharkie/1.IDLE/6.png',
+        '../img/1.Sharkie/1.IDLE/7.png',
+        '../img/1.Sharkie/1.IDLE/8.png',
+        '../img/1.Sharkie/1.IDLE/9.png',
+        '../img/1.Sharkie/1.IDLE/10.png',
+        '../img/1.Sharkie/1.IDLE/11.png',
+        '../img/1.Sharkie/1.IDLE/12.png',
+        '../img/1.Sharkie/1.IDLE/13.png',
+        '../img/1.Sharkie/1.IDLE/14.png',
+        '../img/1.Sharkie/1.IDLE/15.png',
+        '../img/1.Sharkie/1.IDLE/16.png',
+        '../img/1.Sharkie/1.IDLE/17.png',
+        '../img/1.Sharkie/1.IDLE/18.png'
+    ]
     constructor() {
         super().loadImage('../img/1.Sharkie/1.IDLE/1.png')
+        this.loadImages(this.IMAGES_IDLE);
+        this.animateIdle();
     }
     jump() {
+        
+    }
 
+    animateIdle() {
+        
+            setInterval(() => {
+                let i = this.currentImage % this.IMAGES_IDLE.length; //let I = 0 % 6;
+                let path = this.IMAGES_IDLE[i];
+                this.img = this.imageChache[path];
+                this.currentImage++;
+                }, 300);
+        
+        
+        
     }
 }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,16 +4,30 @@ class moveableObject {
     width;
     height;
     img;
+    currentImage = 0;
+    imageChache = {};
+    speed;
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    loadImages(arr) {
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageChache[path] = img;
+        });
+      
+    }
+
      moveRight() {
         console.log('moving right')
     }
-    moveLeft() {
-        console.log('moving left')
+    movementLeft() {
+        setInterval(() => {
+            this.positionx -= this.speed;
+        }, 1000/60);
     }
 }
