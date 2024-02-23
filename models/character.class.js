f1e58ca2e75977d21e869f93d66b74178570c720
Cedric Daunke <daunke40@gmail.com>
2024-02-23 19:05:43 +0100

    fixed swim animation speed

--- models/character.class.js
+++ models/character.class.js
@@ -20,52 +20,82 @@ class Character extends moveableObject {
     "../img/1.Sharkie/1.IDLE/17.png",
     "../img/1.Sharkie/1.IDLE/18.png",
   ];
+  IMAGES_SWIM = [
+    "../img/1.Sharkie/3.Swim/1.png",
+    "../img/1.Sharkie/3.Swim/2.png",
+    "../img/1.Sharkie/3.Swim/3.png",
+    "../img/1.Sharkie/3.Swim/4.png",
+    "../img/1.Sharkie/3.Swim/5.png",
+    "../img/1.Sharkie/3.Swim/6.png",
+  ];
   world;
   constructor() {
     super().loadImage("../img/1.Sharkie/1.IDLE/1.png");
     this.loadImages(this.IMAGES_IDLE);
+    this.loadImages(this.IMAGES_SWIM);
     this.animateIdle();
   }
 
   animateIdle() {
     setInterval(() => {
-        if (!this.world.keyboard.LEFT && !this.world.keyboard.RIGHT && !this.world.keyboard.UP && !this.world.keyboard.DOWN) {
+      if (
+        !this.world.keyboard.LEFT &&
+        !this.world.keyboard.RIGHT &&
+        !this.world.keyboard.UP &&
+        !this.world.keyboard.DOWN
+      ) {
         let i = this.currentImage % this.IMAGES_IDLE.length; //let I = 0 % 6;
         let path = this.IMAGES_IDLE[i];
         this.img = this.imageChache[path];
         this.currentImage++;
-        }
+      }
     }, 250);
   }
-
-  moveRight() {
-   setInterval(() => {
-    if (this.world.keyboard.RIGHT) {
-        console.log("right");
-        this.positionx += 5;
-      }
-   }, 1000 / 60);
+  animateSwim() {
+    
+      let i = this.currentImage % this.IMAGES_SWIM.length; //let I = 0 % 6;
+      let path = this.IMAGES_SWIM[i];
+      this.img = this.imageChache[path];
+      this.currentImage++;
+    
   }
+  
+  moveRight() {
+    // Intervall for Movement
+    setInterval(() => {
+        if (this.world.keyboard.RIGHT) {
+            this.positionx += 5;
+        }
+    }, 1000 / 60);
+
+    // Intervall for Animation
+    setInterval(() => {
+        if (this.world.keyboard.RIGHT) {
+            this.animateSwim();
+        }
+    }, 300); 
+
+   
+}
   moveLeft() {
     setInterval(() => {
-     if (this.world.keyboard.LEFT) {
-         this.positionx -= 5;
-       }
+      if (this.world.keyboard.LEFT) {
+        this.positionx -= 5;
+      }
     }, 1000 / 60);
-   }
-   moveUp() {
+  }
+  moveUp() {
     setInterval(() => {
-     if (this.world.keyboard.UP) {
-         this.positiony -= 5;
-       }
+      if (this.world.keyboard.UP) {
+        this.positiony -= 5;
+      }
     }, 1000 / 60);
-   }
-   moveDown() {
+  }
+  moveDown() {
     setInterval(() => {
-     if (this.world.keyboard.DOWN) {
-         this.positiony += 5;
-       }
+      if (this.world.keyboard.DOWN) {
+        this.positiony += 5;
+      }
     }, 1000 / 60);
-   }
+  }
 }
-
